cd01cd0a112b32edeffc272794c01aa90f64ac6b
TomGrzembke <tomgrzembke04@gmail.com>
2024-04-05 10:57:22 +0100

    Meat picker

---
+++ Assets/+++workdata/Scripts/MeatPicker.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class MeatPicker : MonoBehaviour
+{
+    #region serialized fields
+    [SerializeField] GameObject meatSprite;
+    #endregion
+
+    #region private fields
+    
+    #endregion
+
+}
--- Assets/+++workdata/Scripts/NoodleState.cs
+++ Assets/+++workdata/Scripts/NoodleState.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class NoodleState : MonoBehaviour
@@ -11,12 +12,34 @@ public class NoodleState : MonoBehaviour
     }
 
     #region serialized fields
-    
+    [SerializeField] NoodState noodState;
+    [SerializeField] Sprite noodFilled;
+    [SerializeField] Sprite noodFolded;
+    [SerializeField] SpriteRenderer noodRenderer;
     #endregion
 
     #region private fields
-    
+
     #endregion
 
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Meat"))
+        {
+            collision.gameObject.SetActive(false);
+            noodRenderer.sprite = noodFilled;
+            noodState = NoodState.Unfilled;
+        }
+        if (collision.CompareTag("Steam"))
+        {
+            noodState = NoodState.Steamed;
+            StartCoroutine(SteamDelay());
+        }
+    }
 
+    IEnumerator SteamDelay()
+    {
+        yield return new WaitForSeconds(.7f);
+        noodRenderer.sprite = noodFolded;
+    }
 }
