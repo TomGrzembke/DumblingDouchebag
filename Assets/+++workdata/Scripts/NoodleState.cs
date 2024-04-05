36b82a49e9bdb2a60fbf9ac8593ce4a667b41600
TomGrzembke <tomgrzembke04@gmail.com>
2024-04-05 10:08:30 +0100

    Greifarm

---
+++ Assets/+++workdata/Scripts/NoodleState.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class NoodleState : MonoBehaviour
+{
+    public enum NoodState
+    {
+        Unfilled,
+        FilledAndBound,
+        Steamed,
+        Deployed
+    }
+
+    #region serialized fields
+    
+    #endregion
+
+    #region private fields
+    
+    #endregion
+
+
+}
